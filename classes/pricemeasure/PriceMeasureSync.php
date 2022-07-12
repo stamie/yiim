3ea1690e42b722f300c882c2d7d5c5f724dda196
Emese Ágota Stampel <estampel@gmail.com>
2022-07-12 11:37:32 +0200

    javítás

--- classes/pricemeasure/PriceMeasureSync.php
+++ classes/pricemeasure/PriceMeasureSync.php
@@ -7,7 +7,6 @@ use app\models\Region;
 
 class PriceMeasureSync {
     private static $model = 'app\models\PriceMeasure';
-    
     protected $id;
     protected $wp_id;
     protected $wp_prefix;
@@ -15,7 +14,6 @@ class PriceMeasureSync {
     protected $xml_json_id;
     protected $name;
     protected $is_active;
-
     protected $location_id;
     protected $company_id;
     protected $check_in_time;
@@ -24,26 +22,17 @@ class PriceMeasureSync {
     protected $xml_lat;
     protected $wp_long;
     protected $wp_lat;
-
     /**
      * 
      * PriceMeasure functions 
      */
-
-    public function __construct($ID = null, $wpId = 0, $wpPrefix, $xmlId, $xmlJsonId, $isActive = 1, 
-                                $name
-    ) {
-
+    public function __construct($ID = null, $wpId = 0, $wpPrefix, $xmlId, $xmlJsonId, $isActive = 1, $name) {
         $this->id = $ID;
-        
-        
         $this->xml_id = $xmlId;
         $this->xml_json_id = $xmlJsonId;
         $this->name = $name;
         $this->is_active = intval($isActive);
-
     }
-
     public function getId (){
         return $this->id;
     }
@@ -63,7 +52,6 @@ class PriceMeasureSync {
     public function getIsActive (){
         return $this->is_active;
     }
-
     /**
      * 
      * Additional functions 
@@ -72,57 +60,36 @@ class PriceMeasureSync {
     public function getRegionId (){
         return $this->country_id;
     }
-
     public function getRegion (){
-        
-        $region = Region::findAll(['xml_json_id' => $this->region_id, 'xml_id' => $this->xml_id]);
-        if ($region)
-            return $region[0];
-        return false;
+        $region = Region::findOne(['xml_json_id' => $this->region_id, 'xml_id' => $this->xml_id]);
+        return $region;
     }
-       
     /**
      * 
      * Syncrons function
      */
-    
-     
     public function sync () {
-
-
         if ($this) {
-
             $condition = [
-                
                 'xml_id' => $this->xml_id,
                 'xml_json_id' => $this->xml_json_id,
-
             ];
-
             $object = self::$model::findOne($condition);
             if ($object){
                 $object->is_active = 1;
                 return $object->save(0);
             } else {
                 $object = new self::$model();
-                
-                
-                
                 $object->xml_id = $this->xml_id;
                 $object->xml_json_id = $this->xml_json_id;
                 $object->is_active = 1;
-
                 $object->name = $this->name;
-                
                 if ( $object->save() ) {
                     return $object;
                 }
-
                 var_dump($object); exit;
-                
             }
         }
-
         return false;
     }
 
